a1f320eea2138aeca5424f9fdb27ccf3b8a33954
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-07-02 19:17:07 +0545

    push

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-function getmilk(money){
-        console.log("leaveHouse");
-        console.log("moveRight");
-        console.log("moveRight");
-        console.log("moveUp");
-        console.log("moveUp");
-        console.log("moveUp");
-        console.log("moveUp");
-        console.log("moveRight");
-        console.log("moveRight");
-        let numberofbottles= Math.floor(money/1.5);
+function bmicalculator(weight,height){
+    let bmi = Math.round(weight / (height * height));
+    return bmi;
+}
 
-
-        console.log("buy" +  numberofbottles + "bottles of milk");
-        console.log("moveLeft");
-        console.log("moveLeft");
-        console.log("moveDown");
-        console.log("moveDown");
-        console.log("moveDown");
-        console.log("moveDown");
-        console.log("moveLeft");
-        console.log("moveLeft");
-        console.log("enterHouse");
-        } 
-        getmilk(5);
+  let bmi = bmicalculator(65, 1.8);
+  console.log(bmi);
